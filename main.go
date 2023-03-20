a3a18681cef3cea2647d0f96c5d25af397973051
AlexAR <moltron_nano@hotmail.com>
2023-03-20 16:43:14 -0600

    feat: add initial db connection setup

--- main.go
+++ main.go
@@ -2,14 +2,24 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 /*
    go mod init FiberAuth
    go get github.com/gofiber/fiber/v2
+   go get -u gorm.io/gorm
+   go get -u gorm.io/driver/mysql
 */
 
 func main() {
+
+	_, err := gorm.Open(mysql.Open("root:root@/fiberauthdb"), &gorm.Config{})
+
+	if err != nil {
+		panic("could not connect to the database")
+	}
 
 	app := fiber.New()
 
